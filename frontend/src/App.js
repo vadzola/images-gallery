a41cf91dab2e7622db2aa2924600c89596712107
vadzola <vadzola@yahoo.com.hk>
2022-04-29 18:18:26 +0200

    set  search state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+
+
 function App() {
+  const [word, setWord] = useState('')
+
+const handleSearchSubmit = (e) => {
+  e.preventDefault()
+  console.log(word)
+}
+
   return (
     <div>
-     <h1>Image Gallery</h1>
+     <Header title='Images Gallery' />
+     <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
