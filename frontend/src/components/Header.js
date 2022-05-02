d3fe96e72d9745425746b4b55acfd358e8117e38
vadzola <vadzola@yahoo.com.hk>
2022-05-02 13:59:14 +0200

    End lesson 18

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from './../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#eee',
 };
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
